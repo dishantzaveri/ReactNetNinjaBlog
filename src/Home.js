9ddcb9a4575dfbee7dd9be6a23486c71398e5e81
dishantzaveri <zaveridishant@gmail.com>
2022-12-17 02:29:57 +0530

    hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 const Home = () => {
 
+ const [name, setName] = useState('mario');
+ const [age, setAge] = useState(25);
+
   const handleClick = (e) => {
-    console.log('hello ninjas', e);
+    setName('luigi');
+    setAge(30);
+    
   }
 
   const handleClickAgain = (name, e) => {
@@ -11,6 +18,7 @@ const Home = () => {
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name} is {age} years old </p>
       <button onClick={handleClick}>Click me</button>
       <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
     </div>
